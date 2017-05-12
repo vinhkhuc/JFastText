12c09d53ee60fa2932effaaafc45f3f3e8cf3a5e
Vinh Khuc <knvinh@gmail.com>
2017-05-12 00:54:42 -0700

    Bug fix for failed compiling with fastText.zip.

--- src/main/java/com/github/jfasttext/FastTextWrapper.java
+++ src/main/java/com/github/jfasttext/FastTextWrapper.java
@@ -232,10 +232,12 @@ public class FastTextWrapper extends com.github.jfasttext.config.FastTextWrapper
 
 // #include "fastText/src/args.cc"
 // #include "fastText/src/dictionary.cc"
-// #include "fastText/src/matrix.cc"
-// #include "fastText/src/vector.cc"
-// #include "fastText/src/model.cc"
 // #include "fastText/src/fasttext.cc"
+// #include "fastText/src/matrix.cc"
+// #include "fastText/src/model.cc"
+// #include "fastText/src/productquantizer.cc"
+// #include "fastText/src/qmatrix.cc"
+// #include "fastText/src/vector.cc"
 // #include "fastText/src/utils.cc"
 
 // #include "fasttext_wrapper.cc"
